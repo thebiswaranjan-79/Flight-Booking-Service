33dc4fb6a6d6e7fff265522f9582a5656f8033a3
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-07-13 23:54:29 +0530

    Added Seeders to Insert or Delete Bulk Data in Database

---
+++ src/seeders/20240713181801-add-airplanes.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airplanes', [
+      {
+        modelNumber : 'Mexi340b',
+        capacity : 45,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        modelNumber : 'MoBus10A',
+        capacity : 204,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
